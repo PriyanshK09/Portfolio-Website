8a70e3b1b95d05631d75379b046e1ca3b0c2124a
PriyanshK09 <priyanshkhare4905@gmail.com>
2024-10-16 10:12:11 +0530

    Add initial files for frontend components and styles

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Portfolio from './components/Portfolio';
+
+function App() {
+  return (
+    <div className="App">
+      <Portfolio />
+    </div>
+  );
+}
+
+export default App;
